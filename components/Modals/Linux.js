b40dfe1a57c62dfc29a98c9a7d7777bec68fff65
dante-robinson <danterobinson2000@gmail.com>
2022-07-20 14:48:21 -0400

    Fix navbar Modal

--- components/Modals/Linux.js
+++ components/Modals/Linux.js
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function Linux({setLinuxModalOpen}) {
   return(
     <div className="flex justify-center">
-      <div className="fixed border-2 bg-background border-blue w-48 h-content">
+      <div className="absolute border-2 bg-background border-blue w-48 h-content">
         <ul className="text-center text-sm py-2 space-y-1">
           <li>
             <Link href="/Linux/ArchGuide"><a>Arch Linux Install Guide</a></Link>
