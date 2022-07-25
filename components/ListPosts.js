c54b696ebd75744be12e98229a79d405c073e2f7
dante-robinson <danterobinson2000@gmail.com>
2022-07-25 10:42:53 -0400

    Add performance guide to listposts

--- components/ListPosts.js
+++ components/ListPosts.js
@@ -1,13 +1,19 @@
 import React from "react"
 import ArchGuide from "../pages/Linux/ArchGuide.js"
+import PerformanceGuide from "../pages/Linux/PerformanceGuide.js"
 import Link from "next/link"
 
 export default function ListPosts() {
   return (
-    <div>
+    <div className="space-y-[1.5vh]">
       <div className="border-2 border-blue h-[20vh] mx-[1vw] overflow-hidden">
         <Link href="/Linux/ArchGuide">    
           <a><ArchGuide /></a>
+        </Link>
+      </div>
+      <div className="border-2 border-blue h-[20vh] mx-[1vw] overflow-hidden">
+        <Link href="/Linux/PerformanceGuide">
+          <a><PerformanceGuide /></a>
         </Link>
       </div>
     </div>
