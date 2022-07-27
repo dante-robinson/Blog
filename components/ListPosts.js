c09d237e9bb817ecbaa9bb7cdba3c54912064ffe
dante-robinson <danterobinson2000@gmail.com>
2022-07-27 17:09:20 -0400

    Add security guide to listposts

--- components/ListPosts.js
+++ components/ListPosts.js
@@ -1,13 +1,14 @@
 import React from "react"
 import ArchGuide from "../pages/Linux/ArchGuide.js"
 import PerformanceGuide from "../pages/Linux/PerformanceGuide.js"
+import SecurityGuide from "../pages/Linux/SecurityGuide.js"
 import Link from "next/link"
 
 export default function ListPosts() {
   return (
-    <div className="space-y-[1.5vh]">
+    <div className="space-y-[1.4vh]">
       <div className="border-2 border-blue h-[20vh] mx-[1vw] overflow-hidden">
-        <Link href="/Linux/ArchGuide">    
+        <Link href="/Linux/ArchGuide">
           <a><ArchGuide /></a>
         </Link>
       </div>
@@ -16,6 +17,11 @@ export default function ListPosts() {
           <a><PerformanceGuide /></a>
         </Link>
       </div>
-    </div>
+      <div className="border-2 border-blue h-[20vh] mx-[1vw] overflow-hidden">
+        <Link href="/Linux/SecurityGuide">
+          <a><SecurityGuide /></a>
+        </Link>
+      </div >
+    </div >
   )
 }
