b1ba5a6b06a0fd8749064ea58949ffb91c6f0146
Dante Robinson <danterobinson2000@gmail.com>
2023-05-04 18:36:04 -0400

    Rework Modals

--- includes/Modals/CryptoModal.php
+++ includes/Modals/CryptoModal.php
@@ -2,11 +2,15 @@
     <div class="modal-border">
         <ul class="modal-ul">
             <li class="modal-li">
-                <a class=" link" href="/Crypto/XRPXLMScam">XRP & XLM The biggest scams</a>
-                <br />
-                <a class="link" href="/Crypto/CommunistCBDC">Communist CBDCs</a>
-                <br />
-                <a class="link" href="/Crypto/WhatMakesShtcoins">What makes a Sh*tcoin</a>
+              <a class=" link" href="/Crypto/XRPXLMScam">XRP & XLM The biggest scams</a>
+            </li>
+            <hr>
+            <li class="modal-li">
+              <a class="link" href="/Crypto/CommunistCBDC">Communist CBDCs</a>
+            </li>
+            <hr>
+            <li class="modal-li">
+              <a class="link" href="/Crypto/WhatMakesShtcoins">What makes a Sh*tcoin</a>
             </li>
         </ul>
     </div>
--- includes/Modals/LinuxModal.php
+++ includes/Modals/LinuxModal.php
@@ -2,14 +2,26 @@
     <div class="modal-border">
         <ul class="modal-ul">
             <li class="modal-li">
-                <a class="link" href="/Linux/ArchGuide">Arch Linux Install Guide</a>
-                <br />
-                <a class="link" href="/Linux/PerformanceGuide">Performance Tweaks Guide</a>
-                <br />
-                <a class="link" href="/Linux/SecurityGuide">Linux Security Guide</a>
-                <br />
-                <a class="link" href="/Linux/Gentoomusl">My experience on Gentoo musl</a>
-                <br />
+              <a class="link" href="/Linux/ArchGuide">Arch Linux Install Guide</a>
+            </li>
+            <hr>
+            <li class="modal-li">
+              <a class="link" href="/Linux/PerformanceGuide">Performance Tweaks Guide</a>
+            </li>
+            <hr>
+            <li class="modal-li">
+              <a class="link" href="/Linux/PerformanceGuide">Performance Tweaks Guide</a>
+            </li>
+            <hr>
+            <li class="modal-li">
+              <a class="link" href="/Linux/SecurityGuide">Linux Security Guide</a>
+            </li>
+            <hr>
+            <li class="modal-li">
+              <a class="link" href="/Linux/Gentoomusl">My experience on Gentoo musl</a>
+            </li>
+            <hr>
+            <li class="modal-li">
                 <a class="link" href="/Linux/GentooGlibctomusl">Gentoo Glibc to musl easy</a>
             </li>
         </ul>
